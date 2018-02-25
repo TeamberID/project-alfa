8e58abaf5cee92614db945e928f06b86701214d1
Melnikov Semen <sema.melnikov@gmail.com>
2018-02-25 08:46:52 +0300

    Initial commit. Add gitignore. Add the project's template.

---
+++ project-alfa/src/main/java/ru/kpfu/itis/app/Application.java
@@ -0,0 +1,16 @@
+package ru.kpfu.itis.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Melnikov Semen
+ * 11-601 ITIS KPFU
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
